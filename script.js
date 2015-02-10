ffaf75cb410492e6fa50c83eab1a84f8d917adc8
Sarah Kajani <sarah.kajani@gmail.com>
2015-02-09 22:09:09 -0500

    assignment 5

---
+++ script.js
@@ -0,0 +1,65 @@
+
+// console.log ("hello world");
+// var a = 1
+// var b = -1
+// console.log (a + b)
+// var myName = "Sarah"
+// var lastName = " Kajani"
+// console.log (myName + lastName)
+// var myArray = [["basket", "foot"] , ["ball", "hoe"]]
+// console.log (myArray[0][1])
+// var myArray = ["basket", "ball"]
+// var myBall = ["foot" , "base"]
+// var arrayBall = [myArray, myBall]
+// console.log(arrayBall[1][1])
+// var x = 11
+
+// if (x<10){
+// 	console.log("x is less than 10")
+// 	} else {
+// 	console.log("x is greater than 10")
+// 	}
+// var x = "angels"
+// var y = "badly"
+
+// if (x===y){
+// 	console.log("they are equal")
+// }	else {
+// 	console.log("they are different")
+// }
+// function someName(aName){
+// 	return aName;
+// }
+// console.log(someName("Sarah"))
+// 
+// function doorNumber(prize){
+// 	if (prize === 1) {
+// 		console.log("you won a dog")
+// 	} else if (prize === 2) {
+// 		console.log("you won a cat")
+// 	} else {
+// 		console.log("you lose")	
+// 	}
+// }
+// console.log(doorNumber(3))
+
+function total(number, otherNumber){
+	return number + 5 + otherNumber
+}
+console.log(total(25,10));
+
+function someName(personsName){
+	return alert(personsName);
+}
+someName("Farah");
+
+function wheel(token){
+	if (token === 1){
+		console.log ("you lost!")
+	} else if (token === 2){
+		console.log ("you won!")
+	} else {
+		console.log ("pick your nose")
+	}
+}
+console.log(wheel(2))
---
+++ scripttwo.js
@@ -0,0 +1,4 @@
+function total(number, otherNumber){
+	return number + 5 + otherNumber
+};
+console.log(total(25,10));
